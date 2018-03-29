8b65c411e4005146f5854a023ed60394067ecd9c
yfrans <yfrans@gmail.com>
2018-03-29 22:11:11 +0300

    Added AJAX POST method

--- background.js
+++ background.js
@@ -17,6 +17,18 @@ function jx(url, callback) {
     xmlhttp.send();
 }
 
+function jxp(url, params, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            callback(xmlhttp.responseText);
+        }
+    }
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send(params);
+}
+
 function needUpdate(date) {
     var today = new Date();
     date.setHours(16, 0, 0, 0);
